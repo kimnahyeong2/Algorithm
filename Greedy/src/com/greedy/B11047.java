4d609b06ce5cd098976041953f4ad62db565cf69
NaHyeong <donadio912@naver.com>
2023-07-16 17:57:18 +0900

    2023.07.16 그리디

--- Greedy/src/com/greedy/B11047.java
+++ Greedy/src/com/greedy/B11047.java
@@ -1,10 +1,34 @@
 package com.greedy;
 
+import java.io.*;
+import java.util.*;
+
 public class B11047 {
+	static int N;
+	static int K;
+	static int[] array;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		array = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		int count = 0;
+		for(int i=N-1; i>=0; i--) {
+			if(K >= array[i]) {
+				count += K/array[i];
+				K = K % array[i];
+			}
+		}
+		System.out.println(count);
 	}
 
 }
