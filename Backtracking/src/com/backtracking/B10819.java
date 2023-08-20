2b62d4e1684c07570c20e3a0976721e4a2b5647d
NaHyeong <donadio912@naver.com>
2023-08-20 22:11:27 +0900

    2023.08.20 백트래킹
    10819번 수정

--- Backtracking/src/com/backtracking/B10819.java
+++ Backtracking/src/com/backtracking/B10819.java
@@ -8,31 +8,40 @@ public class B10819 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N; // 정수의 개수
     static int[] array;
-    static int count;
+    static boolean[] visited;
+    static int result = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
     	// 입력
     	N = Integer.parseInt(br.readLine());
     	array = new int[N];
+    	visited = new boolean[N];
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	for(int i=0; i<N; i++) {
     		array[i] = Integer.parseInt(st.nextToken());
     	}
-    	
-    	int max = 0;
-    	
-    	for(int i=0; i<N; i++) {
-    		for(int j=i+1; j<N; j++) {
-    			if(array[i] > array[j]) {
-    				max = array[j];
-    				array[j] = array[i];
-    				array[i] = max;
-    			}
-    		}
-    	}
-    	count = array[0] + array[1];
-    	for(int i=2; i<N; i++) {
-    		count += array[i]*2;
-    	}
-    	System.out.println(count);
+    	ArrayList<Integer> num = new ArrayList<>();
+    	dfs(num, 0);
+    	System.out.println(result);
     }
+	public static void dfs(ArrayList<Integer> num, int count) {
+		if(count == N) {
+			result = Math.max(getResult(num), result);
+		}
+		for(int i=0; i<N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				num.add(array[i]);
+				dfs(num, count+1);
+				num.remove(num.size()-1);
+				visited[i] = false;
+			}
+		}
+	}
+	public static int getResult(ArrayList<Integer> num) {
+		int sum = 0;
+		for(int i=0; i<N-1; i++) {
+			sum += Math.abs(num.get(i)-num.get(i+1));
+		}
+		return sum;
+	}
 }
